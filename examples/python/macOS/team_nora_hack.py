4ad5919428d2b4e87dab71a2bc2e3d100dff9732
kitazaki <kitazaki@gmail.com>
2020-01-26 01:58:10 +0900

    Add files via upload

---
+++ examples/python/macOS/team_nora_hack.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+
+import time
+import uuid
+
+import Adafruit_BluefruitLE
+
+CHARACTERISTIC_SERVICE_UUID = uuid.UUID('0000fee0-0000-1000-8000-00805f9b34fb')
+CHARACTERISTIC_DATA_UUID = uuid.UUID('0000fee1-0000-1000-8000-00805f9b34fb')
+
+provider = Adafruit_BluefruitLE.get_provider()
+
+def main():
+    provider.clear_cached_data()
+    adapter = provider.get_default_adapter()
+    if not adapter.is_powered:
+        adapter.power_on()
+    print('Searching for device...')
+    try:
+        adapter.start_scan()
+        device = provider.find_device(service_uuids=[CHARACTERISTIC_SERVICE_UUID])
+        if device is None:
+            raise RuntimeError('Failed to find device!')
+        else:
+            print(device)
+            print('device: {0}'.format(device.name))
+            print('id: {0}'.format(device.id))
+    finally:
+        adapter.stop_scan()
+    print('Connecting to device...')
+    device.connect()
+    try:
+        print('Discovering services...')
+        device.discover([CHARACTERISTIC_SERVICE_UUID], [CHARACTERISTIC_DATA_UUID])
+        service = device.find_service(CHARACTERISTIC_SERVICE_UUID)
+        print('service uuid: {0}'.format(service.uuid))
+        data = service.find_characteristic(CHARACTERISTIC_DATA_UUID)
+        print('characteristic uuid: {0}'.format(data.uuid))
+        print('Writing Data..')
+        bs = bytes(range(16))
+        bs = b'\x77\x61\x6E\x67\x00\x00\x00\x00\x30\x00\x00\x00\x00\x00\x00\x00'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\x00\x0e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\x00\x00\x00\x00\x00\x00\xE1\x0C\x06\x17\x2D\x23\x00\x00\x00\x00'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\x00\x00\x7e\x18\x18\x18\x18\x18\x18\x18\x00\x00\x00\xfe\xc0\xc0'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\xfe\xc0\xc0\xc0\xfe\x00\x00\x00\xfe\xc6\xc6\xfe\xc6\xc6\xc6\xc6'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\x00\x00\x00\xc6\xee\xfe\xd6\xc6\xc6\xc6\xc6\x00\x00\x00\x00\x00'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc6\xe6\xe6\xf6\xde\xce\xce'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\xc6\x00\x00\x00\xfe\xc6\xc6\xc6\xc6\xc6\xc6\xfe\x00\x00\x00\xfe'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\xc6\xc6\xfe\xd8\xcc\xc6\xc6\x00\x00\x00\xfe\xc6\xc6\xfe\xc6\xc6'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\xc6\xc6\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\xc6\xc6\xc6\xfe\xc6\xc6\xc6\xc6\x00\x00\x00\xfe\xc6\xc6\xfe\xc6'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\xc6\xc6\xc6\x00\x00\x00\xfe\xc6\xc0\xc0\xc6\xc6\xc6\xfe\x00\x00'
+        data.write_value(bs)
+        time.sleep(0.1)
+        bs = b'\x00\xc6\xcc\xd8\xf0\xd8\xcc\xc6\xc6\x00\x00\x00\x00\x00\x00\x00'
+        data.write_value(bs)
+        time.sleep(3)
+        print('Writing done.')
+    finally:
+        device.disconnect()
+
+provider.initialize()
+provider.run_mainloop_with(main)
+
